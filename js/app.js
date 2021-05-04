09fd79119caa3be7f41dea76ddb09659479ca30b
Raj <therajtiwari254@gmail.com>
2021-05-05 01:10:17 +0530

    added login page and react components

--- js/app.js
+++ js/app.js
@@ -1,30 +1,43 @@
-// adult counter
-$("#plusa").click(function () {
-  changeValueA(1);
-});
-$("#minusa").click(function () {
-  changeValueA(-1);
-});
-
-function changeValueA(val) {
-  var container = $("#valuea");
-  var current = parseInt(container.html(), 10);
-
-  container.html(Math.max(0, current + val).toString());
+function printError(elemId, hintMsg) {
+  document.getElementById(elemId).innerHTML = hintMsg;
 }
+function validateLoginForm() {
+  var email = document.loginForm.email.value;
+  var password = document.loginForm.password.value;
 
-// child counter
+  var emailErr = (passwordErr = true);
+  // email
+  if (email == "") {
+    printError("emailErr", "Please enter your email address");
+  } else {
+    // Regular expression for basic email validation
+    var regex = /^\S+@\S+\.\S+$/;
+    if (regex.test(email) === false) {
+      printError("emailErr", "Please enter a valid email address");
+    } else {
+      printError("emailErr", "");
+      emailErr = false;
+    }
+  }
 
-$("#plusc").click(function () {
-  changeValueC(1);
-});
-$("#minusc").click(function () {
-  changeValueC(-1);
-});
+  // password
+  if (password == "") {
+    printError("passwordErr", "Please enter your password");
+  } else {
+    // Regular expression for basic password validation
+    var regex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    if (regex.test(password) === false) {
+      printError("passwordErr", "Please enter a valid password");
+    } else {
+      printError("passwordErr", "");
+      passwordErr = false;
+    }
+  }
 
-function changeValueC(val) {
-  var container = $("#valuec");
-  var current = parseInt(container.html(), 10);
-
-  container.html(Math.max(0, current + val).toString());
+  if ((emailErr || passwordErr) == true) {
+    return false;
+  } else {
+    // console.log("sahiiiiiiiiiiii");
+    alert("You have successfully logged in");
+  }
 }
--- js/main.js
+++ js/main.js
@@ -20,3 +20,4 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+
